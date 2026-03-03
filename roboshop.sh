158aeafbb22f81dcf5393d681b5964dd980bc2fa
Kopparthi Kiran Babu <thekiranbabukopparthi@gmail.com>
2026-03-03 11:42:28 +0530

    new script

--- roboshop.sh
+++ roboshop.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-First we need to know what are the requirements.
-This is the script to create an instance and update the A record in R53.
+# First we need to know what are the requirements.
+# This is the script to create an instance and update the A record in R53.
 
-create an instance. Based on the requirement like whether it is for frontend or backend. 
-we need to get the public ip for frontend instance and private ip for backend instance.
+# create an instance. Based on the requirement like whether it is for frontend or backend. 
+# we need to get the public ip for frontend instance and private ip for backend instance.
 
 
 AMI_ID="ami-0220d79f3f480ecf5"
